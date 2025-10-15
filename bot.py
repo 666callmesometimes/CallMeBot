918546adc7623a3f14b1dd99acb5b14056b26419
👨‍💻Michał Badocha <78705024+666callmesometimes@users.noreply.github.com>
2025-10-15 22:53:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -274,7 +274,7 @@ def run_http_server():
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
-            def log_message(self, *args):
+        def log_message(self, *args):
             return
     PORT = int(os.environ.get("PORT", 10000))
     server = HTTPServer(("", PORT), Handler)
@@ -283,6 +283,7 @@ def run_http_server():
 
 threading.Thread(target=run_http_server, daemon=True).start()
 bot.run(TOKEN)
+
 
 
 
